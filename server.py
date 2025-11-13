447d7549f22d99f0f43a4581243fef7597168bca
Huy Nguyen <nhathuy12@outlook.com>
2025-11-13 14:02:46 +0700

    create login page UI

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import os
 import fastapi
 import uvicorn
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 import psycopg2.pool as psycopg_pool
 import dotenv
 
@@ -14,16 +15,16 @@ app = fastapi.FastAPI()
 pgpool = psycopg_pool.ThreadedConnectionPool(minconn=5, maxconn=60, user=os.getenv("PG_USER"), password=os.getenv("PG_PASSWORD"), host=os.getenv("PG_HOST"), port=os.getenv("PG_PORT"), database=os.getenv("PG_DATABASE"))
 
 # Mount the web dashboard interface
-app.mount("/", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
-def reroute_to_dashboard_ui():
+async def reroute_to_dashboard_ui():
     """
     If the session cookie is present, redirect user to dashboard or admin page.
     Otherwise, redirect to the login page
     """
 
-
+    return RedirectResponse("/static/login.html")
 
 # Run the app
 uvicorn.run(app)
