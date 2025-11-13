bfec4e73575c140abc317fa5e75e6e0e3b2d11aa
Huy Nguyen <nhathuy12@outlook.com>
2025-11-13 13:43:53 +0700

    Set up a minimal web server in FastAPI

---
+++ server.py
@@ -0,0 +1,29 @@
+import os
+
+import fastapi
+import uvicorn
+from fastapi.staticfiles import StaticFiles
+import psycopg2.pool as psycopg_pool
+import dotenv
+
+# Load environment variables
+dotenv.load_dotenv("private.env")
+app = fastapi.FastAPI()
+
+# Set up database connection
+pgpool = psycopg_pool.ThreadedConnectionPool(minconn=5, maxconn=60, user=os.getenv("PG_USER"), password=os.getenv("PG_PASSWORD"), host=os.getenv("PG_HOST"), port=os.getenv("PG_PORT"), database=os.getenv("PG_DATABASE"))
+
+# Mount the web dashboard interface
+app.mount("/", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def reroute_to_dashboard_ui():
+    """
+    If the session cookie is present, redirect user to dashboard or admin page.
+    Otherwise, redirect to the login page
+    """
+
+
+
+# Run the app
+uvicorn.run(app)
