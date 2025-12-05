cfe72dce89958174598692c7b8efc50d8ebeb709
Huy Nguyen <nhathuy12@outlook.com>
2025-12-05 20:08:22 +0700

    create modals for adding user and produces

---
+++ static/settings-admin.js
@@ -0,0 +1,11 @@
+async function createNewUser() {
+    let username = document.getElementById("edit-username-text")
+    let password = document.getElementById("edit-user-password")
+
+    e = await axios.post()
+
+}
+
+function createUserModalToggle(visible) {
+
+}
