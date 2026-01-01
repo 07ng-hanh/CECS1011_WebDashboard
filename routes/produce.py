a77d51cdc02ffbbcba5264e4000c9f840fbdd276
Nguyen Dinh Nhat Huy <huyndn2007@gmail.com>
2026-01-01 22:28:36 +0700

    add endpoints to get and delete produce entry

---
+++ routes/produce.py
@@ -0,0 +1,21 @@
+from fastapi import Depends, APIRouter
+from fastapi.responses import JSONResponse
+from http import HTTPStatus
+from dependency_injection import get_pgpool
+from datamodels import ProduceInfoForm
+import asyncpg
+rt = APIRouter()
+
+@rt.get("/list-produces")
+async def list_produces(page: int = 1, limit: int = 20, query: str = "", pgpool: asyncpg.pool.Pool = Depends(get_pgpool)):
+    async with pgpool.acquire() as con:
+        try:
+            # users = await con.fetch("select username, is_admin from users where username like $1 limit $2 offset $3", f'%{query}%', limit, (page - 1) * limit)
+            # return JSONResponse([{"username": user["username"], "isadmin": user["is_admin"]} for user in users])
+            produces = await con.fetch("select id, harvest_type_name, shelf_life, thresh_temp_lo, thresh_temp_hi, thresh_humidity_lo, thresh_humidity_hi, thresh_co2_lo, thresh_co2_hi from produceinfo where harvest_type_name like $1 limit $2 offset $3",
+                                       f'%{query}%', limit, (page - 1) * limit)
+            produces_json = [[p[0], ProduceInfoForm(harvest_type_name=p[1], shelf_life=p[2], thresh_temp_lo=p[3], thresh_temp_hi=p[4], thresh_humidity_lo=p[5], thresh_humidity_hi=p[6], thresh_co2_lo=p[7], thresh_co2_hi=p[8]).model_dump()] for p in produces]
+            return JSONResponse(produces_json)
+        except BaseException as e:
+            print(e)
+            return JSONResponse({}, HTTPStatus.INTERNAL_SERVER_ERROR)
