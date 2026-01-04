55594cb29e5f105efaf3ba8bcf7590cbaf6cf0c2
Nguyen Dinh Nhat Huy <huyndn2007@gmail.com>
2026-01-04 23:33:22 +0700

    connect frontend to configuration get/set endpoint

--- routes/config.py
+++ routes/config.py
@@ -7,9 +7,13 @@ from datamodels import WarehouseConfig
 rt = APIRouter()
 
 @rt.get("/get-warehouse-config")
-async def get_warehouse_config(keys: list[str], pg: asyncpg.pool.Pool = Depends(get_pgpool)):
+async def get_warehouse_config(keys: str, pg: asyncpg.pool.Pool = Depends(get_pgpool)):
+    # keys: space-delimited keys
     async with pg.acquire() as conn:
-        db_out_raw = await conn.fetch("select key, value from configuration where key = any($1::varchar[])", keys)
+        if keys == "*":
+            db_out_raw = await conn.fetch("select key, value from configuration")
+        else:
+            db_out_raw = await conn.fetch("select key, value from configuration where key = any($1::varchar[])", keys.split())
 
         # Convert data type from string to the appropriate types using a Pydantic BaseModel
         return WarehouseConfig(**{prop[0]: prop[1] for prop in db_out_raw})
