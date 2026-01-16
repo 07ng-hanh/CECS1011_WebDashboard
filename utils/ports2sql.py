805648037613b5941e43abdc0dc70081216175ef
Nguyen Dinh Nhat Huy <huyndn2007@gmail.com>
2026-01-16 23:54:17 +0700

    add port hold time to port data

--- utils/ports2sql.py
+++ utils/ports2sql.py
@@ -1,7 +1,16 @@
 import json
 
 c = json.load(open("../dataset/world_ports.jsonc"))
-sqlstatement = "insert into ports (port_name, port_lat, port_lon, id) values"
+port_hold = open("../dataset/port_hold_time_by_countries.csv").read().split("\n")
+
+port_hold = [x.split(",") for x in port_hold][1:-2]
+
+
+# Median time in port (days) - All ships by World Bank is licensed under CC BY-3.0 IGO
+# Global - International Ports dataset by World Bank is licensed under CC BY-SA 4.0
+port_hold_time_ms = {country.strip(): int(float(hold_days) * 86400 * 1000) for (country, hold_days) in port_hold}
+
+sqlstatement = "insert into ports (port_name, port_lat, port_lon, id, hold_time_ms) values"
 col_inserts = []
 port_ids_cnt = {}
 for feature in c["features"]:
@@ -11,6 +20,7 @@ for feature in c["features"]:
     lat, lon = feature["geometry"]["coordinates"]
     port_id = props["LOCODE"]
 
+    port_hold_time_ms.setdefault(props["Country"], port_hold_time_ms["World"])
 
     # skip ports that are not marine ports
     if "1" not in functions:
@@ -24,7 +34,7 @@ for feature in c["features"]:
     port_ids_cnt[port_id] += 1
     port_name = port_name.replace("'", "''")
 
-    col_inserts.append(f"('{port_name}', {lat}, {lon}, '{port_id}')")
+    col_inserts.append(f"('{port_name}', {lat}, {lon}, '{port_id}', {port_hold_time_ms[props['Country']]})")
 
 f = open('ports2sql-write.txt', 'w')
 f.write(sqlstatement + ", \n".join(col_inserts))
